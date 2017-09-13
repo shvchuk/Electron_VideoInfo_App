5f9d5b3c554fb543fc06a70acdb4fe797e8cc461
Andrzej Szewczuk <andrzej.szewczuk@gmail.com>
2017-09-13 12:47:48 +0200

    show video duration in main window of the application

--- index.js
+++ index.js
@@ -2,16 +2,18 @@ const electron = require('electron');
 const ffmpeg = require('fluent-ffmpeg');
 const { app, BrowserWindow, ipcMain }  = electron;
 
+let mainWindow;
+
 // event base programming
 
 app.on('ready', () => {
-  const mainWindow = new BrowserWindow({});
+  mainWindow = new BrowserWindow({});
   mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
 
 ipcMain.on('video:submit', (event, path) => {
   ffmpeg.ffprobe(path, (err, metadata) => {
-    console.log('Video duration is: ', metadata.format.duration);
+    mainWindow.webContents.send('video:metadata', metadata.format.duration);
   });
 
 });
